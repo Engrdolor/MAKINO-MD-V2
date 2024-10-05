69111a43646a9adf15ed42a41a8e6e869e371a89
Engrdolor <dolorfaithful101@gmail.com>
2024-10-05 20:32:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -18,11 +18,11 @@ global.ANTI_BOT = false;
 global.PM_BLOCKER = false;
 global.CHATBOT = false;
 
-global.SESSION_ID = process.env.SESSION_ID || ""
-global.Owner = ["2349151864541"]; //like 2347080968564 
-global.OwnerName =  "Tᴀɪʀᴀ Mᴀᴋɪɴᴏ";
-global.BotName = "♱MAKINO-MD-V2♱♡⃤";
-global.packname = "♱MAKINO-MD-V2♱♡⃤";                             //Do not change.
+global.SESSION_ID = process.env.SESSION_ID || "taira_tech-W8kAjD5A#xo7Qpvf3KOGg4jLPlYHUE64XlpZcU0i2DP4Nxwg3SQk"
+global.Owner = ["2347087941249"]; //like 2347080968564 
+global.OwnerName =  "Engrdolor";
+global.BotName = "Engrdolor";
+global.packname = "Engrdolor";                             //Do not change.
 global.author = "TAIRA MAKINO";                               //Do not change.
 global.BotSourceCode = "https://github.com/anonphoenix007/MAKINO-MD-V2"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/DOVRqF006VHHZhiSNwJRce"; 
@@ -36,34 +36,34 @@ global.BotLogo = fs.readFileSync("./Assets/pic1.jpg");
 global.Thumb = fs.readFileSync("./Assets/pic7.jpg");
 global.Thumb1 = fs.readFileSync("./Assets/pic5.jpg");
 global.ErrorPic = fs.readFileSync("./Assets/pic7.jpg");
-global.OwnerNumber = [""] //Ignore,Unused
+global.OwnerNumber = ["2347087941249"] //Ignore,Unused
 
 //
-global.ntilinkytvid = []
-global.ntilinkytch = []
-global.ntilinkig = []
-global.ntilinkfb = []
-global.ntilinktg = []
-global.ntilinktt = []
-global.ntilinktwt = []
-global.ntilinkall = []
-global.nticall = []
+global.ntilinkytvid = [true]
+global.ntilinkytch = [true]
+global.ntilinkig = [true]
+global.ntilinkfb = [true]
+global.ntilinktg = [true]
+global.ntilinktt = [true]
+global.ntilinktwt = true[]
+global.ntilinkall = [true]
+global.nticall = [true]
 global.ntwame = []
 global.nttoxic = []
 global.ntnsfw = []
 global.ntvirtex = []
 global.rkyt = []
-global.wlcm = []
-global.gcrevoke = []
-global.autorep = []
-global.ntilink = []
+global.wlcm = [true]
+global.gcrevoke = [true]
+global.autorep = [true]
+global.ntilink = [true]
 
 
 //
 global.mess = {
     jobdone: 'Here you go...',
     useradmin: 'Only group Admin can use the command 😂 ',
-    botadmin: 'Make me Admin first 😌📍.',
+    botadmin: 'Make Engrdolor Admin first 😌📍.',
     botowner: 'Only my *Owner* can use this command!',
     grouponly: 'This command is only made for *Groups*',
     privateonly: 'This command is only made for *Private Chat*',
